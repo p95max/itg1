a16724cae1d1618b789e77597793d770409696a4
Maxx <m.petrykin@gmx.de>
2025-01-22 23:03:52 +0100

    removed rep

--- quiz.py
+++ quiz.py
@@ -3,19 +3,19 @@
 #Question_list
 
 question1 = "What capital of the Great Britain?"
-answer1 = "London"
+answer1a = "London"
 
 question2 = "Who is a current US President?"
-answer2 = "Trump"
+answer2a = "Trump"
 
 question3 = "Which country has the biggest population?"
-answer3 = "India"
+answer3a = "India"
 
 scores = 0
 correct_ans = "Correct "
 wrong_ans = "Wrong!"
 
-if answer1 and answer2 and answer3 == correct_ans:
+if answer1a and answer2a and answer3a == correct_ans:
     scores + 1
 
 #Q1
@@ -23,35 +23,35 @@ if answer1 and answer2 and answer3 == correct_ans:
 print(question1)
 enter_ans = str(input())
 
-if enter_ans == answer1:
+if enter_ans == answer1a:
     print(correct_ans)
-    scores + 1
+    scores += 1
 else:
     print(wrong_ans)
-    scores += 1
+    scores + 1
     
 #Q2
 
 print(question2)
 enter_ans = str(input())
 
-if enter_ans == answer2:
+if enter_ans == answer2a:
     print(correct_ans)
-    scores + 1
+    scores += 1
 else:
     print(wrong_ans)
-    scores += 1
+    scores + 1
     
 #Q3
 
 print(question3)
 enter_ans = str(input())
 
-if enter_ans == answer3:
+if enter_ans == answer3a:
     print(correct_ans)
-    scores + 1
+    scores += 1
 else:
     print(wrong_ans)
-    scores += 1
+    scores + 1
 
 print("Your correct answers: " + str(scores))
