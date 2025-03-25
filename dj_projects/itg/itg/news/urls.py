65e7b0abc87cd6b73e83df19c001b4db5f6debd0
Maxx <m.petrykin@gmx.de>
2025-03-25 11:50:01 +0100

    1

--- dj_projects/itg/itg/news/urls.py
+++ dj_projects/itg/itg/news/urls.py
@@ -1,7 +1,11 @@
+from unicodedata import category
+
 from django.urls import path
-from news.views import get_all_news, get_news_by_id
+from news.views import get_all_news, get_news_by_id, get_category_by_name
+
 
 urlpatterns = [
     path('', get_all_news),
     path('<int:news_id>/', get_news_by_id),
+    path('<str:slug>/', get_category_by_name)
 ]
--- dj_projects/itg/itg/news/views.py
+++ dj_projects/itg/itg/news/views.py
@@ -4,14 +4,26 @@ from django.shortcuts import render
 
 def get_all_news(request):
     info = {
-        'news_count': 100600,
-        'users_count': 1000,
-        'menu': ['Главная', 'О проекте', 'Каталог'],
-
+        "users_count": 100600,
+        "news_count": 100600,
+        "menu": [
+            {"title": "Главная",
+             "url": "/",
+             "url_name": "index"},
+            {"title": "О проекте",
+             "url": "/about/",
+             "url_name": "about"},
+            {"title": "Каталог",
+             "url": "/news/catalog/",
+             "url_name": "catalog"},
+        ]
     }
-    return render(request, 'news/catalog.html')
+    return render(request, 'news/catalog.html', context=info)
 
 def get_news_by_id(request, news_id):
     return HttpResponse(f"Новость {news_id}")
+
+def get_category_by_name(request, slug):
+    return HttpResponse(f"Категория {slug}")
 
 
