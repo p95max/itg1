826902d77df24b8a3da221f188da121b4b889bfe
Maxx <m.petrykin@gmx.de>
2025-03-27 21:50:56 +0100

    itg l5

---
+++ dj_projects/itg/news/urls.py
@@ -0,0 +1,12 @@
+from unicodedata import category
+
+from django.urls import path
+from news.views import get_all_news, get_news_by_id, get_category_by_name, main
+
+
+urlpatterns = [
+    path('', get_all_news),
+    path('<int:news_id>/', get_news_by_id),
+    # path('<str:slug>/', get_category_by_name),
+    path('test/', main)
+]
