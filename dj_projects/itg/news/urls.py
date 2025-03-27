8bcaaf4279914413b6dadb69d3b40610b0b4618e
Maxx <m.petrykin@gmx.de>
2025-03-27 23:22:48 +0100

    search by id and show all news features added

--- dj_projects/itg/news/urls.py
+++ dj_projects/itg/news/urls.py
@@ -1,10 +1,8 @@
-from unicodedata import category
-
 from django.urls import path
-from news.views import get_all_news, get_news_by_id, get_category_by_name, main, article_detail, article_list
+from news.views import main, article_detail, article_list, total_articles
 
 urlpatterns = [
-    path('', get_all_news),
-    path("articles/", article_list, name="article_list"),
-    path("article/<int:id>/", article_detail, name="article_detail"),  # Добавьте этот путь
+    path('', main),                                                     # main page
+    path("articles/", article_list, name="article_list"),               # show all articles
+    path("article/<int:id>/", article_detail, name="article_detail"),   # search by id
 ]
--- dj_projects/itg/news/views.py
+++ dj_projects/itg/news/views.py
@@ -1,48 +1,28 @@
-from django.http import HttpResponse
-
 from django.shortcuts import render
 from .models import Article
 
+def main(request):
+    articles_count = Article.objects.count()                # articles quantity
+    info = {
+        "articles_count": articles_count,
+    }
+    return render(request, 'news/main.html', context=info)
+
 def article_detail(request, id):
     article = Article.objects.get(id=id)
-    return render(request, "article_detail.html", {"article": article})
+    return render(request, "news/article_detail.html", {"article": article})
 
 def article_list(request):
     articles = Article.objects.all()
-    return render(request, "article_list.html", {"articles": articles})
+    return render(request, "news/article_list.html", {"articles": articles})
+
+def total_articles(request):
+    articles_count = Article.objects.count()
+    return render(request, 'news/main.html', {"articles_count": articles_count})
 
 
 
 
 
-
-
-
-def get_all_news(request):
-    info = {
-        "users_count": 100600,
-        "news_count": 100600,
-        "menu": [
-            {"title": "Главная",
-             "url": "/",
-             "url_name": "index"},
-            {"title": "О проекте",
-             "url": "/about/",
-             "url_name": "about"},
-            {"title": "Каталог",
-             "url": "/news/catalog/",
-             "url_name": "catalog"},
-        ]
-    }
-    return render(request, 'news/catalog.html', context=info)
-
-def get_news_by_id(request, news_id):
-    return HttpResponse(f"Новость {news_id}")
-
-def get_category_by_name(request, slug):
-    return HttpResponse(f"Категория {slug}")
-
-def main(request):
-    return render(request, 'main.html')
 
 
