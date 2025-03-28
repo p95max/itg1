5da444907fa402b878f8b41db755c7fef6b0b9d7
Maxx <m.petrykin@gmx.de>
2025-03-28 19:13:27 +0100

    search by id and show all news features added

--- dj_projects/itg/news/urls.py
+++ dj_projects/itg/news/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from news.views import catalog, article_detail, get_all_news, total_articles
+from news.views import catalog
 
 urlpatterns = [
-    path('', catalog),                                                                                                  # main page
-    path("articles/", get_all_news, name="news_list"),                                                               # show all articles
-    path("article/<int:id>/", article_detail, name="article_detail"),                                                   # search by id
-
+    path('catalog/', catalog, name='catalog'),
+    path('catalog/<int:id>/', catalog, name='article_detail'),
 ]
--- dj_projects/itg/news/views.py
+++ dj_projects/itg/news/views.py
@@ -1,30 +1,25 @@
 from django.shortcuts import render
 from .models import Article
 
-def catalog(request):
-    articles_count = Article.objects.count()                                                                            # articles quantity
-    info = {
-        "articles_count": articles_count,
-    }
-    return render(request, 'news/catalog.html', context=info)
+def catalog(request, id=None):
+    if id is None:
+        articles_count = Article.objects.count()
+        news = Article.objects.all().prefetch_related('tags', 'category')
+        context = {
+            "articles_count": articles_count,
+            'news': news,
+        }
+        return render(request, 'news/catalog.html', context=context)
+    else:
+        try:
+            article = Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return render(request, 'news/error.html', {'message': 'Статья не найдена'})
+        return render(request, 'news/article_detail.html', {'article': article})
 
-def article_detail(request, id):
-    article = Article.objects.get(id=id)
-    return render(request, "news/article_detail.html", {"article": article})
 
-# def article_list(request):
-#     articles = Article.objects.all()
-#     return render(request, "news/article_list.html", {"articles": articles})
 
-def get_all_news(request):
-    context = {
-        'news': Article.objects.all().prefetch_related('tags', 'category'),
-    }
-    return render(request, 'news/catalog.html', context)
 
-def total_articles(request):
-    articles_count = Article.objects.count()
-    return render(request, 'news/catalog.html', {"articles_count": articles_count})
 
 
 
