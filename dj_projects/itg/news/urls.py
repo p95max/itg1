bebc790b0d754e74246b490beab177dd8a03a2fa
Maxx <m.petrykin@gmx.de>
2025-03-29 07:55:32 +0100

    some changes

--- dj_projects/itg/news/urls.py
+++ dj_projects/itg/news/urls.py
@@ -1,11 +1,9 @@
-from tkinter.font import names
-
 from django.urls import path
-from news.views import catalog, all_news, article_detail, about_us
+from news.views import catalog, all_news, article_detail, about
 
 urlpatterns = [
     path('', catalog, name='catalog'),
     path('all/', all_news, name='all_news'),
     path('<int:id>/', article_detail, name='article_detail'),
-    path('about/', about_us, name='about_us')
+    path('about/', about, name='about'),
 ]
--- dj_projects/itg/news/views.py
+++ dj_projects/itg/news/views.py
@@ -26,8 +26,17 @@ def article_detail(request, id):
         return render(request, 'news/error.html', {'message': 'Статья не найдена'})
     return render(request, 'news/article_detail.html', {'article': article})
 
-def about_us(request):
-    return render(request, 'news/about_us.html')
+def about(request):
+    context = {
+        'title': 'О нас',
+        'description': 'Мы — команда проекта "Ровные новости", цель которого — предоставлять актуальные и интересные новости.',
+        'contacts': {
+            'email': 'info@rovnyenovosti.ru',
+            'phone': '+7 (123) 456-78-90',
+            'address': 'г. Москва, ул. Примерная, д. 10',
+        },
+    }
+    return render(request, 'news/about.html', context=context)
 
 
 
