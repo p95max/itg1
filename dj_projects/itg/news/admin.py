4d8454c2e2f4875977bb500d4cdac3167e2f6d6a
Maxx <m.petrykin@gmx.de>
2025-04-10 09:11:36 +0200

    admin panel added

--- dj_projects/itg/news/admin.py
+++ dj_projects/itg/news/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article, Category, Tag
+
+def make_inactive(modeladmin, request, queryset):
+    if queryset:
+        queryset.update(is_active=False)
+        make_inactive.short_description = 'Сделать неактивными выбранные статьи'
+
+class TagInline(admin.TabularInline):
+    model = Article.tags.through
+    extra = 1
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'publication_date', 'views', 'is_active')
+    list_filter = ('category', 'is_active')
+    search_fields = ('title', 'content')
+    fields = ('title', 'content', 'category', 'tags', 'is_active')
+    ordering = ('-publication_date',)
+    inlines = [TagInline]
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
--- dj_projects/itg/news/models.py
+++ dj_projects/itg/news/models.py
@@ -37,5 +37,11 @@ class ArticleUserMananger(models.Manager):
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
+    def __str__(self):
+        return self.name
+
 class Tag(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
