37f446f05b590d49ada584a7b359ea7de0f57635
Maxx <m.petrykin@gmx.de>
2025-05-05 12:54:51 +0200

    some changes

--- dj_projects/itg/news/views.py
+++ dj_projects/itg/news/views.py
@@ -1,8 +1,12 @@
+from django.views.generic import ListView
+from django.core import paginator
 from django.http import HttpResponseBadRequest, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 from django.core.paginator import Paginator
 from django.contrib import messages
+from django.views import View
+
 from .forms import ArticleForm
 import json
 from news.models import Article, Tag, Category, Like, Favourite, Comment
