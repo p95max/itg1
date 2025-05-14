f1280c1a12f17abd8acec00a2b60bc1622886c02
Maxx <m.petrykin@gmx.de>
2025-05-14 22:03:44 +0200

    allauth changes

--- dj_projects/itg/itg/settings.py
+++ dj_projects/itg/itg/settings.py
@@ -137,7 +137,7 @@ INTERNAL_IPS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'en-EN'
 
 USE_I18N = True
 USE_L10N = True
